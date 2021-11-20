57a81287820e6fbe39644aee30ab13149e66e3c5
rpi <martin.hermant@gmail.com>
2021-11-20 15:26:54 +0100

    WIP Sat 20 Nov 15:26:54 CET 2021

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -e
-pushd .
-cd schedule
-npm run build
-popd
+#pushd .
+#cd schedule
+#npm run build
+#popd
 cd server
 npm run build
---
+++ commitAll.sh
@@ -0,0 +1,39 @@
+function commitAll(){
+git add -A
+set +e  # Grep succeeds with nonzero exit codes to show results.
+git status | grep committed
+if [ $? -eq 0 ]
+then
+    set -e
+    git status
+    read -r -p "Commit $1? [y/N] " response
+    case "$response" in
+        [yY][eE][sS]|[yY]) 
+            git commit -m "WIP $(date)"
+            git push
+            ;;
+        *)
+            echo "aborting"
+            ;;
+    esac
+else
+    set -e
+    echo "No changes since last run"
+fi
+}
+
+cd server
+commitAll
+
+cd ../rpi-rtc
+commitAll
+
+cd ../Vermuth
+commitAll
+
+
+cd ../omxServer
+commitAll
+
+cd ..
+commitAll
