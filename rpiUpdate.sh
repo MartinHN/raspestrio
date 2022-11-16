e17c39b4f8ca1379a5afd0abda279209ec2d8c55
tinmar <martin.hermant@gmail.com>
2022-11-16 13:22:39 +0100

    improve rpiUpdate

--- rpiUpdate.sh
+++ rpiUpdate.sh
@@ -1,38 +1,47 @@
-# git pull --recurse-submodules
+git pull --recurse-submodules
 #set -e -x
+if [ $(uname -m) == "armv7l" ]; then
+	isPi=1
+fi
 
 function setRW() {
-	sudo mount -o remount,$1 /
+	if [ $isPi ]; then
+		sudo mount -o remount,$1 /
+	else
+		echo "ignore set RW $1"
+	fi
 }
 
-function getCommitBuilt(){
-if [ -f builtCommit ]; then 
-	echo $(cat builtCommit)
-	return
-fi
+function getCommitBuilt() {
+	if [ -f builtCommit ]; then
+		echo $(cat builtCommit)
+		return
+	fi
 }
+
 setRW rw
-for folder in server 
-do
+
+for folder in server; do
 	cd server
 	H=$(git rev-parse HEAD)
 	if [ "$H" != "$(getCommitBuilt)" ]; then
 		echo "rebuilding $folder"
 		./build.sh
 		if [ $? == 0 ]; then
-			echo $H > builtCommit
+			echo $H >builtCommit
 			echo "build completed for $folder"
 		else
 			echo "build failed for $folder"
 		fi
 	else
 		echo "$folder up to date"
-	fi;
+	fi
 	cd ..
-done;
+done
 
 setRW ro
 
-sudo systemctl restart lumestrio
-sudo systemctl restart omxserver
-
+if [ $isPi ]; then
+	sudo systemctl restart lumestrio
+	sudo systemctl restart omxserver
+fi
