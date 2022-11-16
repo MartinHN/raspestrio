dfebd66117b6b5591a15cb93c48b0f9885008fe6
rpi <martin.hermant@gmail.com>
2022-11-16 13:14:16 +0100

    add auto server update

--- rpiUpdate.sh
+++ rpiUpdate.sh
@@ -1,8 +1,37 @@
-git pull --recurse-submodules
+# git pull --recurse-submodules
+#set -e -x
 
-cd server
-npm run build
+function setRW() {
+	sudo mount -o remount,$1 /
+}
 
+function getCommitBuilt(){
+if [ -f builtCommit ]; then 
+	echo $(cat builtCommit)
+	return
+fi
+}
+setRW rw
+for folder in server 
+do
+	cd server
+	H=$(git rev-parse HEAD)
+	if [ "$H" != "$(getCommitBuilt)" ]; then
+		echo "rebuilding $folder"
+		./build.sh
+		if [ $? == 0 ]; then
+			echo $H > builtCommit
+			echo "build completed for $folder"
+		else
+			echo "build failed for $folder"
+		fi
+	else
+		echo "$folder up to date"
+	fi;
+	cd ..
+done;
+
+setRW ro
 
 sudo systemctl restart lumestrio
 sudo systemctl restart omxserver
