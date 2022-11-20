d874f3e28bb7a8637011b0b9d3b7ff10c9cbe683
tinmar <martin.hermant@gmail.com>
2022-11-20 11:57:37 +0100

    update only if changed

--- rpiUpdate.sh
+++ rpiUpdate.sh
@@ -26,7 +26,7 @@ function getCommitBuilt() {
 
 setRW rw
 if [ "$skipFetch" == "0" ]; then
-	git pull local --recurse-submodules
+	git pull local --recurse-submodules=on-demand
 fi
 
 for folder in server schedule Vermuth; do
