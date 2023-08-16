959dfb64cd03309b78c37bada831e9128b7f3b68
tinmar <martin.hermant@gmail.com>
2023-08-16 07:44:28 +0200

    better checkService

--- checkService.sh
+++ checkService.sh
@@ -4,7 +4,11 @@ function printService() {
     echo "--------------"
     echo "$1"
     systemctl status --no-pager $1 | grep 'Active'
-    journalctl -u $1 -n30 --no-pager
+    systemctl is-active --quiet $1
+    isActive=$?
+    if [[ "$isActive" != "0" ]]; then
+        journalctl -u $1 -n30 --no-pager
+    fi
 }
 
 printService lumestrio.service
